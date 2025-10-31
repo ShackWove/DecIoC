27a07e7cea359753c39c42a47844a9915414b4a7
ShackWove <giulio.incoronato01@gmail.com>
2025-10-31 13:07:39 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import ThreatIntelFeed from "./contracts/ThreatIntelFeed.json";
 
-const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // ⚠️ sostituisci
+const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 interface Indicator {
   id: string;
